126596c41d8073b03530a4dfc1b6ada77b7771af
Tiger Bachmeier <tigerbachmeier@gmail.com>
2025-12-15 04:16:18 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
-  measurementId: "G-MEASUREMENT_ID"
+  apiKey: "AIzaSyDXRhUoSX36yOWuzPOGElHh4Vh7Lhbj8lw",
+  authDomain: "studio-8976664643-fedc8.firebaseapp.com",
+  projectId: "studio-8976664643-fedc8",
+  storageBucket: "studio-8976664643-fedc8.appspot.com",
+  messagingSenderId: "693554017260",
+  appId: "1:693554017260:web:bd1e24268edb8dd02c86c9",
+  measurementId: ""
 };
