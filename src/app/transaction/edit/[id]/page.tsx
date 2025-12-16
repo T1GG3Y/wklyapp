71daf6bebd3e23e0c0810d447e85690944c2bc9d
Tiger Bachmeier <tigerbachmeier@gmail.com>
2025-12-16 00:13:37 +0000

    Try fixing this error: `Runtime ReferenceError: buttonVariants is not de

--- src/app/transaction/edit/[id]/page.tsx
+++ src/app/transaction/edit/[id]/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { Button } from '@/components/ui/button';
+import { Button, buttonVariants } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { useFirestore, useUser, useDoc } from '@/firebase';
